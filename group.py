e285afaf02188cc41fa61c96e391228b22777894
刘伟 <liuwei@360huitong.com>
2018-09-25 16:14:38 +0800

    1

--- group.py
+++ group.py
@@ -15,7 +15,7 @@ def group_text(msg):
                 if menb['NickName'] == "hhh":#从群成员列表找到用户,只转发他的消息
                     from_user = menb['UserName']
                     break
-        if g['NickName'] == 'hhh':#把消息发到这个群
+        if g['NickName'] == 'qqq':#把消息发到这个群
             to_group = g['UserName']
     if msg['FromUserName'] == from_group:
         if msg['ActualUserName'] == from_user:
