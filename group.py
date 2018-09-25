1042eea9a6761067457b91a59de76cbf57adf001
刘伟 <liuwei@360huitong.com>
2018-09-25 16:09:13 +0800

    1

---
+++ group.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+import itchat
+from itchat.content import TEXT
+from itchat.content import *
+
+@itchat.msg_register(TEXT, isGroupChat=True)
+def group_text(msg):
+    group  = itchat.get_chatrooms(update=True)
+    from_user = ''
+    for g in group:
+        if g['NickName'] == 'hhh':#从群中找到指定的群聊
+            from_group = g['UserName']
+            for menb in g['MemberList']:
+                #print(menb['NickName'])
+                if menb['NickName'] == "张利晓":#从群成员列表找到用户,只转发他的消息
+                    from_user = menb['UserName']
+                    break
+        if g['NickName'] == 'hhh':#把消息发到这个群
+            to_group = g['UserName']
+    if msg['FromUserName'] == from_group:
+        if msg['ActualUserName'] == from_user:
+            itchat.send('%s:%s'%(msg['ActualNickName'],msg['Content']),to_group)
+itchat.auto_login(hotReload=False)
+itchat.run()
