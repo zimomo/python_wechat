645f37e6ad9f5962d819b789c3b1ab0c2f22e8db
刘伟 <liuwei@360huitong.com>
2018-09-26 15:37:29 +0800

    1

--- wechat.py
+++ wechat.py
@@ -3,16 +3,17 @@ from wxpy import *
 import time
 bot = Bot(console_qr=True, cache_path=True)
 
+print(bot)
 # 机器人账号自身
-while True:
-	myself = bot.self
-	# 查找昵称为'乙醚。'的好友
-	#my_friend = bot.friends().search(u'小小世界')[0]
-	my_group = bot.groups().search(u'hhh')[0]
+# while True:
+# 	myself = bot.self
+# 	# 查找昵称为'乙醚。'的好友
+# 	#my_friend = bot.friends().search(u'小小世界')[0]
+# 	my_group = bot.groups().search(u'狗年旺德福')[0]
 
-	#my_friend.send('Hello, WeChat!')
-	my_group.send('Hello, WeChat!')
-	#休眠十秒
-	time.sleep(10)
-	# 向文件传输助手发送消息
-	#bot.file_helper.send('Hello from wxpy!')
+# 	#my_friend.send('Hello, WeChat!')
+# 	my_group.send('我爱北京天安门')
+# 	#休眠十秒
+# 	time.sleep(60)
+# 	# 向文件传输助手发送消息
+# 	#bot.file_helper.send('Hello from wxpy!')
