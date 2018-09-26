62ac1a09f0257d2b5877e4d7f8128a8ffd82c901
刘伟 <liuwei@360huitong.com>
2018-09-26 16:02:32 +0800

    1

--- wechat.py
+++ wechat.py
@@ -4,22 +4,20 @@ import time
 bot = Bot(console_qr=True, cache_path=True)
 
 print(bot)
-Bot.join()
-embed()
 print(bot['Bot'])
 # 机器人账号自身
-# while True:
-# 	if bot == '<Bot: 丶子沫沫>':
-# 		myself = bot.self
-# 		# 查找昵称为'乙醚。'的好友
-# 		#my_friend = bot.friends().search(u'小小世界')[0]
-# 		my_group = bot.groups().search(u'狗年旺德福')[0]
+while True:
+	if bot is not None:
+		myself = bot.self
+		# 查找昵称为'乙醚。'的好友
+		#my_friend = bot.friends().search(u'小小世界')[0]
+		my_group = bot.groups().search(u'狗年旺德福')[0]
 
-# 		#my_friend.send('Hello, WeChat!')
-# 		my_group.send('我爱北京天安门')
-# 		#休眠十秒
-# 		time.sleep(60)
-# 		# 向文件传输助手发送消息
-# 		#bot.file_helper.send('Hello from wxpy!')
-# 	else:
-# 		bot = Bot(console_qr=True, cache_path=True)
+		#my_friend.send('Hello, WeChat!')
+		my_group.send('我爱北京天安门')
+		#休眠十秒
+		time.sleep(60)
+		# 向文件传输助手发送消息
+		#bot.file_helper.send('Hello from wxpy!')
+	else:
+		bot = Bot(console_qr=True, cache_path=True)
