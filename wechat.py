abacacec584df884cdc6c4b54070bb43078f4262
刘伟 <liuwei@360huitong.com>
2018-09-26 16:19:03 +0800

    1

--- wechat.py
+++ wechat.py
@@ -6,11 +6,12 @@ bot = Bot(console_qr=True, cache_path=True)
 while True:
 		myself = bot.self
 		# 查找昵称为'乙醚。'的好友
-		my_friend = bot.friends().search(u'小小世界')[0]
-		my_group = bot.groups().search(u'狗年旺德福')[0]
+		#my_friend = bot.friends().search(u'小小世界')[0]
+		my_group = bot.groups().search(u'hhh')[0]
 		print(my_group)
-		my_friend.send('Hello, WeChat!')
-		my_group.send('我爱北京天安门')
+		#my_friend.send('Hello, WeChat!')
+		content = '我爱北京天安门';
+		my_group.send(content)
 		#休眠十秒
 		time.sleep(60)
 		# 向文件传输助手发送消息
