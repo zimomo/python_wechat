4f27386666dae3e941fcc27f92db6f954e0b468c
刘伟 <liuwei@360huitong.com>
2018-09-26 16:59:00 +0800

    1

--- wechat.py
+++ wechat.py
@@ -7,6 +7,7 @@ print bot, type(bot)
 # 机器人账号自身
 while True:
 		if bot is not None:
+			print bot, type(bot)
 			myself = bot.self
 			# 查找昵称为'乙醚。'的好友
 			#my_friend = bot.friends().search(u'小小世界')[0]
@@ -16,7 +17,7 @@ while True:
 			content = '我爱北京天安门';
 			my_group.send(content)
 			#休眠十秒
-			time.sleep(60)
+			time.sleep(10)
 			# 向文件传输助手发送消息
 			#bot.file_helper.send('Hello from wxpy!')
 		else:
