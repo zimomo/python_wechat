2963e0e4ea262e649e564e344f15820cd3a201ca
刘伟 <liuwei@360huitong.com>
2018-09-26 15:12:26 +0800

    1

--- wechat.py
+++ wechat.py
@@ -3,12 +3,13 @@ from wxpy import *
 bot = Bot(console_qr=True, cache_path=True)
 
 # 机器人账号自身
-myself = bot.self
-# 查找昵称为'乙醚。'的好友
-my_friend = bot.friends().search(u'小小世界')[0]
-my_group = bot.groups().search(u'hhh')[0]
+while True:
+	myself = bot.self
+	# 查找昵称为'乙醚。'的好友
+	#my_friend = bot.friends().search(u'小小世界')[0]
+	my_group = bot.groups().search(u'hhh')[0]
 
-my_friend.send('Hello, WeChat!')
-my_group.send('Hello, WeChat!')
-# 向文件传输助手发送消息
-bot.file_helper.send('Hello from wxpy!')
+	#my_friend.send('Hello, WeChat!')
+	my_group.send('Hello, WeChat!')
+	# 向文件传输助手发送消息
+	#bot.file_helper.send('Hello from wxpy!')
