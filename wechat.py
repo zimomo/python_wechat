3c73ee4d04855fc0e8b717e283a206ff436f98d3
刘伟 <liuwei@360huitong.com>
2018-09-26 15:04:51 +0800

    1

--- wechat.py
+++ wechat.py
@@ -6,7 +6,9 @@ bot = Bot(console_qr=True, cache_path=True)
 myself = bot.self
 # 查找昵称为'乙醚。'的好友
 my_friend = bot.friends().search(u'小小世界')[0]
+my_group = bot.groups().search(u'hhh')[0]
 
 my_friend.send('Hello, WeChat!')
+my_group.send('Hello, WeChat!')
 # 向文件传输助手发送消息
 bot.file_helper.send('Hello from wxpy!')
