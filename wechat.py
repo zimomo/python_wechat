a9560a677851d7af03001103e501a8a7b07553c8
刘伟 <liuwei@360huitong.com>
2018-09-26 16:12:16 +0800

    1

--- wechat.py
+++ wechat.py
@@ -7,11 +7,11 @@ print(bot)
 # 机器人账号自身
 while True:
 	if bot is not None:
-		myself = bot.self
+		#myself = bot.self
 		# 查找昵称为'乙醚。'的好友
 		#my_friend = bot.friends().search(u'小小世界')[0]
 		my_group = bot.groups().search(u'狗年旺德福')[0]
-
+		print(my_group)
 		#my_friend.send('Hello, WeChat!')
 		my_group.send('我爱北京天安门')
 		#休眠十秒
