f705ec727a7fbc7500608c064478ab22d47edcd1
刘伟 <liuwei@360huitong.com>
2018-09-26 14:16:03 +0800

    1

--- toone.py
+++ toone.py
@@ -2,7 +2,8 @@
 import itchat
 itchat.auto_login()
 users=itchat.search_friends("冰糖葫芦娃哈哈~")
+print(users)
 userName= users[0]['NickName']
 print(userName)
-itchat.send('你好冰糖葫芦娃哈哈~',toUserName=userName)
+#itchat.send('你好冰糖葫芦娃哈哈~',toUserName=userName)
 
