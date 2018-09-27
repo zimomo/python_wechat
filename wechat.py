5fd91582d4ea45d5fb1b69700dc7ebb643a01a26
刘伟 <liuwei@360huitong.com>
2018-09-27 13:49:27 +0800

    1

--- wechat.py
+++ wechat.py
@@ -49,7 +49,7 @@ while True:
 		print(_queue_shield_wechat)
 		print len (_queue_shield_wechat)
 		#my_friend.send('Hello, WeChat!')
-		if _queue_shield_wechat != '':
+		if len(_queue_shield_wechat) > 0:
 		#content = '住进布达拉宫，\n我是雪域最大的王。\n流浪在拉萨街头，\n我是世间最美的情郎。'
 			content = _queue_shield_wechat
 			my_group.send(content)
