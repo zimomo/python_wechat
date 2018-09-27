460576196f13b04f09db966fbda20869a0c729e8
刘伟 <liuwei@360huitong.com>
2018-09-27 13:32:10 +0800

    1

--- wechat.py
+++ wechat.py
@@ -41,10 +41,10 @@ while True:
 		_queue_shield_wechat = shield_redis.blpop(redis_queue['shield']['queueShieldWechat'], 0)[1]
 		print(_queue_shield_wechat)
 		#my_friend.send('Hello, WeChat!')
-		#if len (_queue_shield_wechat):
+		if len (_queue_shield_wechat):
 		#content = '住进布达拉宫，\n我是雪域最大的王。\n流浪在拉萨街头，\n我是世间最美的情郎。'
-		content = _queue_shield_wechat
-		my_group.send(content)
+			content = _queue_shield_wechat
+			my_group.send(content)
 		#休眠十秒
 		time.sleep(10)
 		# 向文件传输助手发送消息
