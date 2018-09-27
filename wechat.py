94740d96f080c68a5be800552d77e15acd3b8986
刘伟 <liuwei@360huitong.com>
2018-09-27 16:02:29 +0800

    1

--- wechat.py
+++ wechat.py
@@ -4,20 +4,20 @@
 实时收集屏蔽警告数据并推送至指定微信群
 """
 from __future__ import unicode_literals
-#from conf.settings import *
+from conf.settings import *
 from wxpy import *
-#import os
-#import sys
+import os
+import sys
 
 import time
 import redis
 
-# curPath = os.path.abspath(os.path.dirname(__file__))
-# rootPath = os.path.split(curPath)[0]
-# sys.path.append(rootPath)
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 
-# reload(sys)
-# sys.setdefaultencoding('utf8')
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 
 
@@ -52,8 +52,8 @@ while True:
 	# print len (_queue_shield_wechat)
 	# #my_friend.send('Hello, WeChat!')
 	# if len(_queue_shield_wechat) > 0:
-	#content = '住进布达拉宫，\n我是雪域最大的王。\n流浪在拉萨街头，\n我是世间最美的情郎。'
-	content = '1111111'
+	content = '住进布达拉宫，\n我是雪域最大的王。\n流浪在拉萨街头，\n我是世间最美的情郎。'
+	#content = '1111111'
 	my_group.send(content)
 	#休眠十秒
 	time.sleep(10)
